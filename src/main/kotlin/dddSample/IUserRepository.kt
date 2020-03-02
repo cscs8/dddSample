54ef4c58983eb28fb349620fc6d62fe7ab0d35ef
cs8 <42040439+cscs8@users.noreply.github.com>
2020-03-02 22:38:56 +0900

    add UserRepository

---
+++ src/main/kotlin/dddSample/IUserRepository.kt
@@ -0,0 +1,7 @@
+import dddSample.User
+import dddSample.UserName
+
+interface IUserRepository {
+    fun find(username: UserName): User
+    fun save(user: User)
+}
---
+++ src/main/kotlin/dddSample/UserRepository.kt
@@ -0,0 +1,13 @@
+import dddSample.User
+import dddSample.UserName
+
+class UserRepository : IUserRepository {
+    override fun find(username: UserName): User {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun save(user: User) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
